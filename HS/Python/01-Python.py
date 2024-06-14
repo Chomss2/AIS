6235a30a1a62ee1e05804842ea9d15f5471f1f4e
Chomss2 <endy.tauleigne@gmail.com>
2024-06-14 15:35:36 +0200

    MAJ Pyth

--- HS/Python/01-Python.py
+++ HS/Python/01-Python.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-
+    #Endy T
 #Modules
 import requests
 import urllib
@@ -13,51 +13,86 @@ import numpy as np
 from get_certificate_chain.download import SSLCertificateChainDownloader
 from scapy.all import *
 #Variables Request
-req = requests.get
-url = "https://taisen.fr"
-res = req(url)
+def fReq() :
+    req = requests.get
+    url = "https://taisen.fr"
+    res = req(url)
+    print(res)
+
 
 #IP et Nom du serveur DNS
-stream = os.popen('nslookup taisen.fr')
-output = stream.read().strip()
+def fDNS() :
+    stream = os.popen('nslookup taisen.fr')
+    output = stream.read().strip()
+    print("IP et nom du serveur DNS : "+output)
 
 #IP et Port Source et Destination
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect(("taisen.fr", 443))
-SourceDest = socket
-print(socket.close)
-socket.close()
+def fIPports() :
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("taisen.fr", 443))
+    SourceDest = sock
+    print("IP Source et Destination")
+    print(sock.close)
+    sock.close()
 
 #Afficher les headers/leur utilité
-html_content = res.text 
-soup = BeautifulSoup(html_content,"html.parser") 
-
-##Array balises
-array = [tag.name for tag in soup.find_all()]
-array = np.array(array)
+def fHeaders() :
+    html_content = res.text 
+    soup = BeautifulSoup(html_content,"html.parser") 
+    print("Liste des headers")
+    print(soup)
+    ##Array balises
+    array = [tag.name for tag in soup.find_all()]
+    # array = np.array(array)
+    print("Tableau des headers")
+    print(array)
 
 ##Elements du certificat
-urlcert = "www.office.com"
-certbrut = ssl.get_server_certificate((urlcert, 443))
-cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, certbrut)
-cert.get_subject().get_components() 
-#Chaine du certificat
-dlchaine = SSLCertificateChainDownloader()
-dlchaine.run({'host': urlcert})
-chaine = dlchaine.run({'host': urlcert})
-#Liste des équipements 
-hostname = "taisen.fr"
-for i in range(1, 28):
-    pkt = IP(dst=hostname, ttl=i) / TCP(dport=33434)
-    # Send the packet and get a reply
-    reply = sr1(pkt, verbose=0)
-    if reply is None:
-        # No reply =(
-        break
-    elif reply.type == 3:
-        # We've reached our destination
-        print ("Done!", reply.src)
-        break
-    else:
-        # We're in the middle somewhere'
-        print ("%d hops away: " % i , reply.src)
+def fCert() :
+    urlcert = "www.office.com"
+    certbrut = ssl.get_server_certificate((urlcert, 443))
+    cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, certbrut)
+    print(cert.get_subject().get_components()) 
+
+    #Chaine du certificat
+    dlchaine = SSLCertificateChainDownloader()
+    dlchaine.run({'host': urlcert})
+    chaine = dlchaine.run({'host': urlcert})
+    print(chaine)
+#Liste des équipements
+def fTracerout () : 
+    hostname = "office.com"
+    for i in range(1, 28):
+        pkt = IP(dst=hostname, ttl=i) / TCP(dport=33434)
+        # Send the packet and get a reply
+        reply = sr1(pkt, verbose=0)
+        if reply is None:
+            # No reply =(
+            print("Erreur")
+            break
+        elif reply.type == 3:
+            # We've reached our destination
+            print ("Done!", reply.src)
+            break
+        else:
+            # We're in the middle somewhere'
+          print ("%d hops away: " % i , reply.src)
+####
+
+if __name__ == "__main__" :
+
+    #Requête
+    fReq()
+    #IP et Nom du serveur DNS
+    fDNS()
+    #IP source et destination
+    fIPports()
+    #Afficher les Headers et les balises
+    req = requests.get
+    url = "https://taisen.fr"
+    res = req(url)
+    fHeaders()
+    #Afficher les informations du certificat et la chaine de confiance
+    fCert()
+    #Afficher les IP des équipements traversés 
+    fTracerout()
